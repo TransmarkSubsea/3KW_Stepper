53c08e44a357e1960075ce5153117020d8287d7b
AzureAD\NigelMoney_linfnde <nigel@krona-subsea.com>
2026-04-28 12:36:36 +0100

    File structure adjust

---
+++ 3kW_Stepper/stepper.h
@@ -0,0 +1,14 @@
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+//define stepper pins
+#define Stepper_EN 16
+#define Stepper_DIR 17
+#define Stepper_PULSE 18
