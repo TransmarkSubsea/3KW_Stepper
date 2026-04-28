63eefc16d7370cbf81335f562ebe246da1e4aa5f
AzureAD\NigelMoney_linfnde <nigel@krona-subsea.com>
2026-04-28 12:32:03 +0100

    Testing code to make motor turn

---
+++ 3kW_Stepper/3kW_Stepper.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/watchdog.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+
+
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+//define stepper pins
+#define Stepper_EN 16
+#define Stepper_DIR 17
+#define Stepper_PULSE 18
+
+
+int main()
+{
+    stdio_init_all();
+
+    // Watchdog example code
+    if (watchdog_caused_reboot()) {
+        printf("Rebooted by Watchdog!\n");
+        // Whatever action you may take if a watchdog caused a reboot
+    }
+    
+    // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
+    // second arg is pause on debug which means the watchdog will pause when stepping through code
+    //watchdog_enable(100, 1);
+    
+    // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
+    //watchdog_update();
+
+    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
+    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
+    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+    
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
+    
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    //Set-up stepper pins
+    gpio_init(Stepper_EN);
+    gpio_set_dir(Stepper_EN, GPIO_OUT);
+    gpio_init(Stepper_DIR);
+    gpio_set_dir(Stepper_DIR, GPIO_OUT);
+    gpio_init(Stepper_PULSE);
+    gpio_set_dir(Stepper_PULSE, GPIO_OUT);
+
+    gpio_put(Stepper_EN, 0); //Enable stepper driver
+    gpio_put(Stepper_DIR, 0); //Set direction to forward
+    gpio_put(Stepper_PULSE, 0); //Set pulse pin low
+
+  while (true) 
+  {
+    printf("Starting stepper pulse sequence: 1000 toggles with 50ms delay\n");
+
+    for(int i = 0; i < (3200*4); i++) 
+    {
+        gpio_put(Stepper_PULSE, !gpio_get(Stepper_PULSE)); // Toggle the pulse pin
+        sleep_us(35); // 50ms delay between transitions
+    }
+
+    printf("Stepper pulse sequence complete\n");
+    gpio_put(Stepper_DIR, !gpio_get(Stepper_DIR));
+
+    sleep_ms(2000);
+    }
+}
